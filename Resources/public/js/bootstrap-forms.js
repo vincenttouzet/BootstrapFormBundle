551b3117ca54196834576ef1e402510d917ad8c9
Vincent Touzet <vincent.touzet@gmail.com>
2013-04-14 21:29:42 +0200

    Fix JS

--- Resources/public/js/bootstrap-forms.js
+++ Resources/public/js/bootstrap-forms.js
@@ -1,27 +1,28 @@
 (function($){
-    
-    /**
-     * DatePicker
-     */
-    $('.bootstrap-datepicker').each(function(){
-        var $input = $('input', this);
-        var week_start = $input.data('week-start');
-        var view_mode = $input.data('view-mode');
-        var min_view_mode = $input.data('min-view-mode');
-        $(this).datepicker({
-            format: 'yyyy-mm-dd',
-            weekStart: week_start,
-            viewMode: view_mode,
-            minViewMode: min_view_mode
+    $(document).ready(function(){
+        /**
+         * DatePicker
+         */
+        $('.bootstrap-datepicker').each(function(){
+            var $input = $('input', this);
+            var week_start = $input.data('week-start');
+            var view_mode = $input.data('view-mode');
+            var min_view_mode = $input.data('min-view-mode');
+            $(this).datepicker({
+                format: 'yyyy-mm-dd',
+                weekStart: week_start,
+                viewMode: view_mode,
+                minViewMode: min_view_mode
+            });
         });
-    });
-    /**
-     * TimePicker
-     */
-    $('.bootstrap-timepicker input').each(function(){
-        $(this).timepicker({
-            showMeridian: false,
-            defaultTime: false
+        /**
+         * TimePicker
+         */
+        $('.bootstrap-timepicker input').each(function(){
+            $(this).timepicker({
+                showMeridian: false,
+                defaultTime: false
+            });
         });
     });
 })(jQuery);
